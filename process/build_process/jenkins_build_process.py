6ddd90667770e60426926b4b43cc2bd28b663a2b
SeptemberHX <tianjianshan@outlook.com>
2019-01-06 17:48:26 +0800

    add log for Jenkins build process

--- process/build_process/jenkins_build_process.py
+++ process/build_process/jenkins_build_process.py
@@ -41,14 +41,18 @@ class JenkinsBuildProcess(base_build_process.BaseBuildProcess):
                 build_info.docker_image_tag
             )
             self.job = JenkinsBuildProcess.J.create_job(build_info.project_name, job_config)
+            JenkinsBuildProcess.log.debug('Job {0} created'.format(self.job.name))
         else:
             self.job = JenkinsBuildProcess.J.get_job(build_info.project_name)
+            JenkinsBuildProcess.log.debug('Job exists')
 
     def run_job(self):
         if self.job:
             JenkinsBuildProcess.J.build_job(self.job.name)
             JenkinsBuildProcess.log.debug(
                 '{0} build #{1} started'.format(self.job.name, self.job.get_next_build_number()))
+        else:
+            JenkinsBuildProcess.log.debug('Job is None')
 
     def get_job_status(self):
         status = None
@@ -57,11 +61,13 @@ class JenkinsBuildProcess(base_build_process.BaseBuildProcess):
             last_build = self.job.get_last_build()
         except NoResults as e:
             JenkinsBuildProcess.log.debug('NoResults exception happened')
-            status = 'FAIL'
+            status = 'PENDING'
         except Exception as e2:
             JenkinsBuildProcess.log.error(e2)
 
         if last_build:
             status = last_build.get_status()
+            if status is None:
+                status = 'NOT_KNOW'
         JenkinsBuildProcess.log.debug('Build {0}'.format(status))
         return status
